5fb00a4de0413b0ef923ceedd493d6b875e04c7b
Seljehaug <cs_seljehaug@hotmail.com>
2017-12-18 21:44:01 +0100

    setup

---
+++ js/custom.js
@@ -0,0 +1,112 @@
+var $mmenu,
+	mmenuapi,
+	currentPosition = 0;
+
+function parallaxObj($o, speed) {
+	"use strict";
+	$o.css('transform', 'translate3d(0, ' + ~~(currentPosition / speed) + 'px, 0)');
+}
+
+function fadeInObjects() {
+	"use strict";
+	var t = $(window).scrollTop(),
+		e = $(window).height();
+	$(".fade").not(".in").each(function() {
+		var $fadeinelem = $(this);
+		var n = $fadeinelem.offset().top - e,
+			i = (e / 8);
+		if (t - i > n) {
+			if ($fadeinelem.data('fadedelay')) {
+				setTimeout(function() {
+					$fadeinelem.addClass("in");
+				}, parseInt($fadeinelem.data('fadedelay')) * 100);
+			} else {
+				$fadeinelem.addClass("in");
+			}
+		} else if (t < n) {
+			/* $(this).removeClass("in"); */
+		}
+	});
+}
+
+function validateEmail($email) {
+	var emailReg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,6})?$/;
+	return emailReg.test($email);
+}
+
+function validateFormField($obj) {
+	"use strict";
+	var valid = 1;
+	$obj.parent().addClass('fb_invalid');
+	if (($obj.val() === '') || (($obj.attr('type') == 'email') && (!validateEmail($obj.val())))) {
+		valid = 0;
+	} else {
+		$obj.parent().removeClass('fb_invalid');
+	}
+	return valid;
+}
+
+function validateForm($formObj) {
+	"use strict";
+	var valid = 1;
+	$formObj.find(".required").each(function() {
+		if (validateFormField($(this).children('input, textarea, select').first()) === 0) {
+			valid = 0;
+		}
+		$(this).children('input, textarea, select').unbind().blur(function() {
+			validateFormField($(this));
+		});
+	});
+	if (valid === 1) {
+    // 	console.log("VALID: Submit");
+		$formObj.attr("action", $formObj.attr("action") + '#' + $formObj.closest('.page').attr('id'));
+		$formObj.submit();
+	} else {
+		// console.log("NOT VALID");
+  }
+}
+
+$(function() {
+	"use strict";
+	currentPosition = $(window).scrollTop();
+
+	// --- Mobile menu ---
+	$mmenu = $("#mobilemenu").mmenu({
+		offCanvas: {
+			position: "right"
+		},
+		navbar: {
+			add: false,
+			title: "Meny",
+			titleLink: "Tilbake"
+		}
+	}, {});
+
+	mmenuapi = $mmenu.data("mmenu");
+
+	// --- Form validation ---
+	$('form.validate').find('input[type=submit]').on('click', function(e) {
+		validateForm( $(this).closest('form') );
+		return false;
+	});
+
+	fadeInObjects();
+
+	$(window).scroll(function() {
+	});
+
+});
+
+$(window).scroll(function() {
+	"use strict";
+	currentPosition = $(window).scrollTop();
+
+	fadeInObjects();
+});
+
+$(window).resize(function() {
+	"use strict";
+	currentPosition = $(window).scrollTop();
+
+	fadeInObjects();
+});
---
+++ js/mmenu/js/menu-fix.js
@@ -0,0 +1,42 @@
+// $(function() {
+// 	var width = $( window ).width();
+//
+// 	$("#mobilemenu_show a").click(function(){
+		// if (width < 700) {
+		// 	console.log("clicked");
+		// 	$("#mobilemenu").css("display", "block");
+		// }
+		// if (width > 700) {
+		// 	console.log("clicked over");
+		// 	$("#mobilemenu").css("display", "none");
+		// }
+		// console.log(width);
+
+		// if opened, display block
+		// if not, display none
+		// mm-opened
+
+		// if($("#mobilemenu").hasClass("mm-opened")){
+		// 	console.log("open");
+		// }
+		//
+		// if($("#mobilemenu").not("mm-opened")){
+		// 	console.log("not open");
+		// }
+		// else{
+		// 	$("#mobilemenu").css("display", "none");
+		// }
+
+
+
+		// $("#mobilemenu").css("display", "block");
+
+		// $( window ).resize(function() {
+		// 	console.log($( window ).width());
+		// 	if($( window ).width() < 700){
+		// 		$("#mobilemenu").css("display", "none");
+		// 	}
+		// });
+
+// 	});
+// });
