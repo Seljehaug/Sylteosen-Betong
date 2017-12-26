be1f64897150257e722b94c66d466851e1a9cf8e
Seljehaug <cs_seljehaug@hotmail.com>
2017-12-26 13:02:07 +0100

    bugfixes

--- js/menu.js
+++ js/menu.js
@@ -1,36 +1,51 @@
 $(function(){
+	// If you want the page to not be able to scroll when menu is active,
+	// comment out the two lines where overflow is changed for body,html
 
 	// Mobile menu
 	var menu = $('#menu-mobile');
-	var width = menu.width();
-
 	var toggle = $('.toggle-menu');
+
 	toggle.on('click', function(){
 		if(menu.hasClass('active')){
+			var w_width = $(window).width();
+
 			menu.removeClass('active');
-			// toggle.removeClass('is-open');
-			// toggle.addClass('is-closed');
 			// $('body, html').css('overflow', 'auto');
 			$('.toggle-menu .menu-icon').css('background-image', 'url("../img/icons/menu-blue.svg")');
-			toggle.css({'left': '-75px', 'transition': 'left 0.15s ease-in-out'});
+			if(w_width > 500){
+				toggle.css({'left': '-27%', 'transition': 'left 0.15s ease-in-out'});
+			}else{
+				toggle.css({'left': '-22%', 'transition': 'left 0.15s ease-in-out'});
+			}
 
 		}else{
 			menu.addClass('active');
-			// toggle.addClass('is-open');
-			// toggle.removeClass('is-closed');
 			// $('body, html').css('overflow', 'hidden');
 			$('.toggle-menu .menu-icon').css('background-image', 'url("../img/icons/close-white.svg")');
 			toggle.css({'left': '1rem', 'transition': 'left 0.15s ease-in-out'});
 		}
 	});
 
-	// Active state
+	// Active state in menu
 	var loc = window.location.href; // returns the full URL
-
 	var url_parts = loc.split('/');
 	var current_page = url_parts.pop() || url_parts.pop(); // handle potential trailing slash
-
 	var selector = '#menu-normal a[href*="' + current_page + '"]';
+
 	$(selector).addClass('active');
 	$('#menu-mobile a[href*="' + current_page + '"]').addClass('active');
+});
+
+// Fix the correct left margin for the menu icon (when not active)
+$(window).resize(function() {
+	if(! $('#menu-mobile').hasClass('active') ){
+		var toggle = $('.toggle-menu');
+		var w_width = $(window).width();
+		if(w_width > 500){
+			toggle.css({'left': '-27%', 'transition': 'left 0.15s ease-in-out'});
+		}else{
+			toggle.css({'left': '-22%', 'transition': 'left 0.15s ease-in-out'});
+		}
+	}
 });
